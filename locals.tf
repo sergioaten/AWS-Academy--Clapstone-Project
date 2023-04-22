0c7bb7e9f5e164b128f396a58cd02929916e4c0a
Sergio <sergio@git.com>
2023-04-22 22:39:40 +0200

    v1 - working

--- locals.tf
+++ locals.tf
@@ -1,21 +1,21 @@
 
 locals{
-    ssm_ps = {
-        1 = {
+    ssm_ps = [
+        {
             name                = "/example/endpoint",
             value_dbinstance    = aws_db_instance.mysql_db.address
         },
-        2 = {
+        {
             name                = "/example/username",
             value_dbinstance    = aws_db_instance.mysql_db.username
         },
-        3 = {
+        {
             name                = "/example/password",
             value_dbinstance    = aws_db_instance.mysql_db.password
         },
-        4 = {
+        {
             name                = "/example/database",
             value_dbinstance    = aws_db_instance.mysql_db.db_name
         }    
-    }
+    ]
 }
